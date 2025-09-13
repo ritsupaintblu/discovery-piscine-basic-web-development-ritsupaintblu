421d12016a28e289535ca9fe495b984d7479286b
ritsupaintblu <aaomm9493@gmail.com>
2025-09-13 12:32:34 +0700

    iii

--- cell03/ex04/ex01bis/script.js
+++ cell03/ex04/ex01bis/script.js
@@ -1,16 +1,43 @@
 $(document).ready(function() {
-    $("#balloon").click(function() {
-        // เปลี่ยนลูกโป่งให้เหมือนระเบิด
-        $(this).css({
-            width: "0",
-            height: "0",
-            backgroundColor: "transparent",
-            boxShadow: "none"
-        });
+  let size = 200; 
+  let colors = ['#ffd1ec', '#ffbae2ff', '#fa95d0ff']; 
+  let colorIndex = 0;
 
-        // เอาลูกโป่งออกจาก DOM หลังจาก 1 วินาที
-        setTimeout(() => {
-            $(this).remove();
-        }, 1000);
+  const $balloon = $("#balloon");
+
+  // เมื่อคลิก
+  $balloon.on("click", function() {
+    size += 10;
+    colorIndex = (colorIndex + 1) % colors.length;
+
+    $balloon.css({
+      width: size + "px",
+      height: size + "px",
+      backgroundColor: colors[colorIndex]
     });
+
+    if (size > 420) {
+      size = 200;
+      colorIndex = 0;
+      $balloon.css({
+        width: size + "px",
+        height: size + "px",
+        backgroundColor: colors[colorIndex]
+      });
+    }
+  });
+
+  // เมื่อเอาเมาส์ไปวางบนลูกโป่ง
+  $balloon.on("mouseenter", function() {
+    if (size > 200) {
+      size -= 5;
+      colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+
+      $balloon.css({
+        width: size + "px",
+        height: size + "px",
+        backgroundColor: colors[colorIndex]
+      });
+    }
+  });
 });
